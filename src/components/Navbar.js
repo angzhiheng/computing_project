d650b9133471926a2729862b3c6d5c455c041a99
Onn <cheeonn8@protonmail.com>
2021-09-29 12:31:37 +0800

    Navbar & Homepage Setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Sunrise Laundry Machine</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/reservation">Reservation</a>
+        <a href="/account">Account</a>
+      </div>
+    </nav>
+  );
+}
+ 
+export default Navbar;
